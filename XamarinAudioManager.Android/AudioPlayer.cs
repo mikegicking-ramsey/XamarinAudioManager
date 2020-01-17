662a49255a510ec23cdf439323e027b4a3a51fe8
Mike Gicking <mike.gicking@daveramsey.com>
2020-01-17 13:50:15 -0600

    Initial commit

---
+++ XamarinAudioManager.Android/AudioPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace XamarinAudioManager
+{
+    public class AudioPlayer
+    {
+        public AudioPlayer()
+        {
+        }
+    }
+}
---
+++ XamarinAudioManager.Android/XamarinAudioManagerBroadcastReceiver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Android.Content;
+using Android.Views;
+
+namespace XamarinAudioManager
+{
+    [BroadcastReceiver(Enabled = true, Exported = true)]
+    public class XamarinAudioManagerBroadcastReceiver : BroadcastReceiver
+    {
+        public XamarinAudioManagerBroadcastReceiver()
+        {
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent.Action == "music-controls-media-button")
+            {
+                var buttonPressed = (KeyEvent)intent.GetParcelableExtra(Intent.ExtraKeyEvent);
+                if (buttonPressed.Action == KeyEventActions.Up)
+                {
+                    switch (buttonPressed.KeyCode)
+                    {
+                        case Keycode.MediaPause:
+                        case Keycode.MediaPlay:
+                        case Keycode.MediaPlayPause:
+                            AudioPlayer.SharedInstance.PlayPause();
+                            break;
+                        case Keycode.MediaNext:
+                        case Keycode.MediaFastForward:
+                        case Keycode.MediaSkipForward:
+                        case Keycode.MediaStepForward:
+                            AudioPlayer.SharedInstance.SeekForward();
+                            break;
+                        case Keycode.MediaPrevious:
+                        case Keycode.MediaRecord:
+                        case Keycode.MediaSkipBackward:
+                        case Keycode.MediaStepBackward:
+                            AudioPlayer.SharedInstance.SeekBackward();
+                            break;
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("OnReceive hit!");
+            }
+        }
+    }
+}
